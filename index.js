ca2c02b5ef42a5149d295da42fc9e8c0d7b5e4c7
elegant-history-c012 <momcurated@gmail.com>
2021-02-18 10:23:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,9 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }
-}
 
-function sayHiToGrandma(string) {
   if (string.toUpperCase === string) {
     return "YES INEED!"
   }
